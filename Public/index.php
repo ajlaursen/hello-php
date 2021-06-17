5fee0392fc41644e6d00f5b1b97d28ea8f62e50b
Alex Laursen <contact@ajlaursen.com>
2021-06-17 15:58:33 -0400

    extending classes

--- Public/index.php
+++ Public/index.php
@@ -13,36 +13,42 @@
 
 
   <?php
-class Movie
+class Chef
 {
-    public $title;
-    private $rating;
-
-    public function __construct($title, $rating)
+    public function makeChicken()
     {
-        $this->title = $title;
-        $this->setRating($rating);
-
+        echo "the chef makes chicken <br>";
     }
 
-    public function showRating()
+    public function makeSalad()
     {
-        echo $this->rating;
+        echo "the chef makes Salad <br>";
     }
-    public function setRating($rating)
+
+    public function makeSpecialDish()
     {
-        if ($rating == "R" || $rating == "PG-13" || $rating == "G" || $rating == "NR" || $rating == "PG") {
-            $this->rating = $rating;
-        } else {
-            $this->rating = "NR";
-        }
+        echo "the chef makes BBQ Ribs <br>";
     }
 }
 
-$movie1 = new Movie("Avengers", "DOG");
+class ItalianChef extends Chef
+{
+    public function makePasta()
+    {
+        echo "the chef makes pasta <br>";
+    }
+    public function makeSpecialDish()
+    {
+        echo "the chef makes lasagna <br>";
+    }
+}
 
-$movie1->setRating("PG-13");
-$movie1->showRating();
+$chef = new Chef;
+$chef->makeSpecialDish();
+
+$chef2 = new ItalianChef;
+$chef2->makeSpecialDish();
+$chef2->makePasta()
 
 ?>
 
