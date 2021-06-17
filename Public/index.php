3c907c1d913a05b98fe9397cb82c53a3e8280f2b
Alex Laursen <contact@ajlaursen.com>
2021-06-17 08:49:35 -0400

    arrays checkboxes

--- Public/index.php
+++ Public/index.php
@@ -10,11 +10,27 @@
 
 <body>
 
+  <form action="index.php" method="post">
+
+
+    Apples: <input type="checkbox" name="fruits[]" value="apples" />
+    <br>
+    Oranges: <input type="checkbox" name="fruits[]" value="oranges" />
+    <br>
+    pears: <input type="checkbox" name="fruits[]" value="pears" />
+    <br>
+    <input type="submit" />
+
+  </form>
+  <br>
+
   <?php
 
-  $friends = array("mike", "karen", "jim", "alex");
-  $friends[10] = "art";
-  echo count($friends);
+  $fruits = $_POST["fruits"];
+  echo $fruits[0];
+
+
+
 
   ?>
 
