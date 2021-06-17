011fd0623c31fff061ae9f8eb1a2eecaebe1ffb7
Alex Laursen <contact@ajlaursen.com>
2021-06-17 09:04:40 -0400

    functions

--- Public/index.php
+++ Public/index.php
@@ -10,25 +10,19 @@
 
 <body>
 
-  <!-- <form action="index.php" method="post">
 
-
-    Apples: <input type="checkbox" name="fruits[]" value="apples" />
-    <br>
-    Oranges: <input type="checkbox" name="fruits[]" value="oranges" />
-    <br>
-    pears: <input type="checkbox" name="fruits[]" value="pears" />
-    <br>
-    <input type="submit" />
-
-  </form>
-  <br> -->
 
   <?php
 
-  $grades = array("jim" => "A+", "pam" => "b");
-  echo $grades["jim"];
+  function sayHi($user)
+  {
+    echo "hello $user <br>";
+  };
 
+
+  sayHi("Mike");
+
+  sayHi("bob")
 
 
 
