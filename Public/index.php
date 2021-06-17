5bf0a003d050129133728e482071126bf6b01c23
Alex Laursen <contact@ajlaursen.com>
2021-06-17 09:40:04 -0400

    if comparisons

--- Public/index.php
+++ Public/index.php
@@ -16,18 +16,20 @@
 
 
 
-  function getMax($num1, $num2)
+  function getMax($num1, $num2, $num3)
   {
-    if ($num1 > $num2) {
-      echo "$num1 is bigger than $num2<br>";
+    if ($num1 > $num2 && $num1 > $num3) {
+      echo "$num1 is bigger than $num2 and $num3<br>";
+    } elseif ($num2 > $num1 && $num2 > $num3) {
+      echo "$num2 is bigger than $num1 and $num3<br>";
     } else {
-      echo "$num1 is smaller than $num2<br>";
+      echo "$num3 is bigger than $num2 and $num1<br>";
     }
   };
 
-  getMax(20, 30);
-  getMax(40, 30);
-  getMax(500, 5000)
+  getMax(20, 30, 15);
+  getMax(40, 30, 14);
+  getMax(500, 5000, 17)
 
 
   ?>
