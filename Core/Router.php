bd3521a4413adb5e41b257601c8d2b9ab9c244bf
Alex Laursen <contact@ajlaursen.com>
2021-06-18 14:41:57 -0400

    regex and variable paramaters

--- Core/Router.php
+++ Core/Router.php
@@ -7,8 +7,16 @@ class Router
 
     protected $params = [];
 
-    public function add($route, $params)
+    public function add($route, $params = [])
     {
+        $route = preg_replace('/\//', '\\/', $route);
+
+        $route = preg_replace('/\{([a-z]+)\}/', '(?P<\1>[a-z-]+)', $route);
+
+        $route = preg_replace('/\{([a-z]+):([^\}]+)\}/', '(?P<\1>\2)', $route);
+
+        $route = '/^' . $route . '$/i';
+
         $this->routes[$route] = $params;
     }
 
@@ -19,12 +27,29 @@ class Router
 
     public function match($url)
     {
+
+        // foreach ($this->routes as $route => $params) {
+        //     if ($url == $route) {
+        //         $this->params = $params;
+        //         return true;
+        //     }
+        // }
+
+        // $regExp = "/^(?P<controller>[a-z-]+)\/(?P<action>[a-z-]+)$/";
         foreach ($this->routes as $route => $params) {
-            if ($url == $route) {
+
+            if (preg_match($route, $url, $matches)) {
+
+                foreach ($matches as $key => $match) {
+                    if (is_string($key)) {
+                        $params[$key] = $match;
+                    }
+                }
                 $this->params = $params;
                 return true;
             }
         }
+        return false;
     }
 
     public function getParams()
--- Public/article-header.php
+++
@@ -1,3 +0,0 @@
-<h2><?php echo $title; ?></h2>
-<h2><?php echo $author; ?></h2>
-word count: <?php echo $wordCount; ?>
