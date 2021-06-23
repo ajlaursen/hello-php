78da4c487bdd8aeba570fcc1d3046372a8661b75
Alex Laursen <contact@ajlaursen.com>
2021-06-23 11:56:38 -0400

    PDO db connections

--- App/Models/Post.php
+++ App/Models/Post.php
@@ -3,22 +3,34 @@
 namespace App\Models;
 
 use PDO;
-use PDOException;
 
-class Post
+/**
+ * Post model
+ *
+ * PHP version 5.4
+ */
+class Post extends \Core\Model
 {
 
+  /**
+   * Get all the posts as an associative array
+   *
+   * @return array
+   */
   public static function getAll()
   {
-    $host = '127.0.0.1';
-    $dbname = 'MVC';
-    $username = 'root';
-    $password = 'rootpass';
+    //$host = 'localhost';
+    //$dbname = 'mvc';
+    //$username = 'root';
+    //$password = 'secret';
 
     try {
-      $db = new PDO("mysql host=$host;dbname=$dbname;charset=utf8", $username, $password);
+      //$db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8",
+      //              $username, $password);
+      $db = static::getDB();
 
-      $stmt = $db->query('SELECT id, title, content FROM posts | ORDER BY created_at');
+      $stmt = $db->query('SELECT id, title, content FROM posts 
+                                ORDER BY created_at');
       $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
       return $results;
